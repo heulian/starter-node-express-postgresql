8f1a4e2ab4b0faa22d9c6a7f735605c94bcc962d
Your Name <heulian@gmail.com>
2021-04-26 15:27:25 -0400

    PUT /suppliers/:supplierId endpoint; Add read() and update()to suppliers.service.js

--- src/suppliers/suppliers.service.js
+++ src/suppliers/suppliers.service.js
@@ -9,6 +9,26 @@ function create(supplier){
 }
 //insert() method can be used to insert more than one resors, so it returns an array of the records inserted.
 //For this API only one supplie will ever be inserted at a time so you chain .then((createdRecords)=>createdRecords[0]) to return only the one inserted record
+
+
+//-----Put /supplier/:supplierID-----//
+//Ability to update an existing supplier in the database
+//You can use Knex update() to update a row in the table.  Update() method accepts as its
+//argument an object that contains the data for updating the existing supplier
+//1.add read and update() to suppliers.service; You are creating read so you can use this function
+//for validation in the route handlers later on 
+function read(supplier_id){
+    return knex("suppliers").select("*").where({supplier_id}).first()
+    }
+
+function update(updatedSupplier){
+    return knex("suppliers")
+    .select("*")
+    .where ({suppplier_id: updatedSupplier.supplier_id})
+    .update(updatedSupplier, "*")
+    //.then((updatedRecords) => updatedRecords[0]); } it says to do this not sure why?
+}
+
 module.exports={
     create,
 }
