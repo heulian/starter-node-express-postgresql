b230cd441346c8d2199956f9c7b92918f827ea03
Your Name <heulian@gmail.com>
2021-04-26 13:26:52 -0400

    Validate params for productId with regex added in products/ router

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -2,7 +2,9 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
+//Validate :productId with regex; to ensure that a route param :productId in path
+//products/prodyctId consists of just one or more digits, prepend the regex expression
+//([0-9]+) to end of path 
 router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
 module.exports = router;
